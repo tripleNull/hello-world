cbe55add7aee14209790a09ad7f7c4bcfe03fd7f
tripleNull <tripleNull@protonmail.com>
2017-11-14 00:42:17 -0500

    Added string replacement with variable values

--- hello.rs
+++ hello.rs
@@ -2,4 +2,11 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello World!");
+    let mut age: i32 = 30;
+
+    let is_it_true: bool = true;
+
+    let let_x: char = 'x';
+
+    println!("I am {0} {1} {2}", age, let_x, is_it_true);
 }
